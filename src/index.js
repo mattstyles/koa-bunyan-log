c3ba2b0b7541755a7f4799b2829677bdac223668
Matt Styles <matt@veryfizzyjelly.com>
2015-09-11 17:02:23 +0100

    #add Logger

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 
-export default 'Hooray!'
+import bunyan from 'bunyan'
+
+export default class Logger {
+    constructor( opts ) {
+        opts = Object.assign({
+            logOpts: {
+                name: 'koa'
+            },
+            logger: null
+        }, opts )
+
+        this.logger = logger || bunyan.createLogger( opts.logOpts )
+    }
+
+    attach( opts ) {
+
+    }
+}
